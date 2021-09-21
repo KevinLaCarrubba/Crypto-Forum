6b52c991752207d5f07aba4718a473e77760e0d5
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-09-21 17:03:44 -0400

    fixed delete comments

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -40,15 +40,15 @@ const showOldComments = async (event) => {
     .then(function (response) {
       return response.json();
     })
-    .then(function (comment) {
-      // console.log(comment[0].body);
-      var commentData = [];
-      for (i = 0; i < comment.length; i++) {
-        var commentLoop = comment[i].body;
-        commentData.push(commentLoop);
-      }
-      return commentData;
-    })
+    // .then(function (comment) {
+    //   console.log(comment);
+    //   var commentData = [];
+    //   for (i = 0; i < comment.length; i++) {
+    //     var commentLoop = comment[i].body;
+    //     commentData.push(commentLoop);
+    //   }
+    //   return commentData;
+    // })
     .then(function (renderComment) {
       renderComment.forEach((comment) => {
         console.log(comment);
@@ -57,7 +57,15 @@ const showOldComments = async (event) => {
         var text = document.createElement("textarea");
         text.rows = 3;
         text.classList.add("form-control", "commentbox");
-        text.innerHTML = comment;
+        text.innerHTML = comment.body;
+
+        var deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.addEventListener("click", function (event) {
+          event.preventDefault();
+          deleteComments(comment.id);
+        });
+        commentHolderDiv.appendChild(deleteBtn);
         div.appendChild(text);
         commentHolderDiv.appendChild(div);
       });
@@ -77,7 +85,24 @@ const newCommentAdd = async (event) => {
   });
 };
 
-function renderComments() {
+function deleteComments(id) {
+  console.log("hello");
+  // needs to change the URL dynamcally based where its being hosted.
+  // this wont work in Heroku because it is hard coded to localhost
+
+  fetch("http://localhost:3001/api/comments/" + id, {
+    method: "delete",
+  })
+    .then(function (data) {
+      console.log(data);
+      location.reload();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function renderComments(event) {
   event.preventDefault();
   var newComment = newCommentid.value;
 
@@ -88,6 +113,13 @@ function renderComments() {
   textArea.classList.add("form-control", "commentbox");
   // textArea.setAttribute("readonly");
   textArea.innerHTML = newComment;
+  var deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    deleteComments(comments.id);
+  });
+  commentHolderDiv.appendChild(deleteBtn);
   commentDiv.appendChild(textArea);
   commentHolderDiv.appendChild(commentDiv);
   newCommentAdd();
